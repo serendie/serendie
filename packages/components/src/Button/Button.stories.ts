0e199a2104f56849b976d08ebbe86b457f7541ac
nagayama <nagayama@iroirodesign.com>
2024-01-25 10:57:00 +0900

    setup pandacss

--- packages/components/src/Button/Button.stories.ts
+++ packages/components/src/Button/Button.stories.ts
@@ -13,9 +13,7 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  argTypes: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -24,27 +22,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: "Button",
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: "Button",
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: "small",
-    label: "Button",
+    children: "Button",
   },
 };
