a6f1d7896785c641257439c21debe557f3d7e9c4
putchom <putchom8@gmail.com>
2024-03-09 23:10:18 +0900

    Read tokens files

--- design-tokens/src/sync_tokens_to_figma.ts
+++ design-tokens/src/sync_tokens_to_figma.ts
@@ -1,5 +1,15 @@
+import * as fs from "fs";
+import { readJsonFiles } from "./token_import.js";
+
 const main = () => {
-  console.log("Hello, Figma!");
+  const TOKENS_DIR = "tokens";
+  const tokensFiles = fs
+    .readdirSync(TOKENS_DIR)
+    .map((file: string) => `${TOKENS_DIR}/${file}`);
+
+  const tokensByFile = readJsonFiles(tokensFiles);
+
+  console.log("Read tokens files:", Object.keys(tokensByFile));
 };
 
 main();
