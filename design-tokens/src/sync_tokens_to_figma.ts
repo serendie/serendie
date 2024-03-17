87070ea14c6385ed5bff40ab01d3b1d7f4d94ba6
putchom <putchom8@gmail.com>
2024-03-17 20:16:29 +0900

    Get local variables from Figma

--- design-tokens/src/sync_tokens_to_figma.ts
+++ design-tokens/src/sync_tokens_to_figma.ts
@@ -1,7 +1,18 @@
+import "dotenv/config";
 import * as fs from "fs";
+
+import FigmaApi from "./figma_api.js";
+
 import { readJsonFiles } from "./token_import.js";
 
-const main = () => {
+const main = async () => {
+  if (!process.env.PERSONAL_ACCESS_TOKEN || !process.env.FILE_KEY) {
+    throw new Error(
+      "PERSONAL_ACCESS_TOKEN and FILE_KEY environemnt variables are required"
+    );
+  }
+  const fileKey = process.env.FILE_KEY;
+
   const TOKENS_DIR = "tokens";
   const tokensFiles = fs
     .readdirSync(TOKENS_DIR)
@@ -10,6 +21,11 @@ const main = () => {
   const tokensByFile = readJsonFiles(tokensFiles);
 
   console.log("Read tokens files:", Object.keys(tokensByFile));
+
+  const api = new FigmaApi(process.env.PERSONAL_ACCESS_TOKEN);
+  const localVariables = await api.getLocalVariables(fileKey);
+
+  console.log("Local variables:", localVariables);
 };
 
 main();
