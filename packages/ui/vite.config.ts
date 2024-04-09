5f59af644f414c331369a6589dda1fd2ab70a3a1
nagayama <nagayama@iroirodesign.com>
2024-04-09 13:57:34 +0900

    build a css file

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -6,20 +6,19 @@ export default defineConfig({
   plugins: [react(), svgr()],
   build: {
     lib: {
-      entry: "src/index.ts",
-      name: "Spread", // グローバル変数としてアクセスする際の名前
-      fileName: "index", // 出力されるファイル名
+      entry: ["src/index.ts", "src/styles.css"],
+      name: "Spread",
     },
     rollupOptions: {
       // 外部化する依存関係
       external: ["react", "react-dom"],
       output: {
-        // グローバル変数として依存関係を利用するための設定
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
         },
       },
     },
+    cssCodeSplit: true,
   },
 });
