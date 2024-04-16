510e71165e67b7632e30185c22ef9eff5130d116
nagayama <nagayama@iroirodesign.com>
2024-04-16 17:44:01 +0900

    update storybook to 8.0.x

---
+++ packages/ui/src/components/Button.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "../components/Button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  parameters: {
+    controls: {
+      expanded: true,
+      include: ["children", "variant", "size"],
+    },
+  },
+  argTypes: {
+    variant: {
+      options: ["primary", "secondary", "tertiary"],
+      control: { type: "radio" },
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {
+    children: "Button",
+  },
+};
