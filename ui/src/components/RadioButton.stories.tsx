f175fa506017f8cf8af9143ae03afb63af73b11d
kenshir0f <kenshir0f.design@gmail.com>
2024-04-29 00:16:40 +0900

    Implement expand and compact style

--- ui/src/components/RadioButton.stories.tsx
+++ ui/src/components/RadioButton.stories.tsx
@@ -1,16 +1,40 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { RadioButton } from "./RadioButton"
+import { RadioButton, RadioButtonProps } from "./RadioButton"
+import { RadioGroup } from "@ark-ui/react";
 
 const meta: Meta<typeof RadioButton> = {
   component: RadioButton,
+  decorators: [
+    (Story) => (
+      <RadioGroup.Root>
+        <Story />
+      </RadioGroup.Root>
+    )
+  ]
 }
+
 
 export default meta;
 type Story = StoryObj<typeof RadioButton>;
 
-export const Medium: Story = {
+const Template= (args: RadioButtonProps) => (
+  <RadioGroup.Root>
+    <RadioButton {...args} label="タイトルタイトル1" value="itemA" />
+    <RadioButton {...args} label="タイトルタイトル2" value="itemB" />
+    <RadioButton {...args} label="タイトルタイトル3" value="itemC" disabled={true} />
+    <RadioButton {...args} label="タイトルタイトル4" value="itemD" />
+  </RadioGroup.Root>
+);
+
+
+export const Expanded: Story = {
+  render: Template,
+};
+
+export const Compact: Story = {
+  render: Template,
   args: {
-    children: "RadioButton",
+    variant: "compact",
   },
 };
--- ui/src/components/RadioButton.tsx
+++ ui/src/components/RadioButton.tsx
@@ -1,51 +1,99 @@
-import { RadioGroup } from '@ark-ui/react'
+import {
+  RadioGroup,
+  RadioGroupItemControlProps,
+  RadioGroupItemProps,
+  RadioGroupItemTextProps,
+} from "@ark-ui/react";
 
-import { styled } from "../../styled-system/jsx";
+import { RecipeVariantProps, sva } from "../../styled-system/css";
 
-const RadioGroupItemControl = styled(RadioGroup.ItemControl, {
+export const RadioButtonStyle = sva({
+  slots: ["item", "itemControl", "itemText"],
   base: {
-    background: "transparent",
-    borderColor: "dic.system.color.component.outline",
-    borderRadius: "dic.system.dimension.radius.full",
-    borderWidth: "dic.system.dimension.border.medium",
-    height: "16px",
-    width: "16px",
-    margin: "4px",
-    _checked: {
-      background: "dic.system.color.impression.primary",
-      outlineColor: "dic.system.color.component.inverseOnSurface",
-      outlineStyle: "solid",
-      outlineWidth: "2px",
-      outlineOffset: "-3px",
-      borderColor: "dic.system.color.impression.primary"
+    item: {
+      alignItems: "center",
+      display: "flex",
+      cursor: "pointer",
+      gap: "dic.system.dimension.spacing.small",
+      paddingY: "dic.system.dimension.spacing.small",
+      paddingX: "dic.system.dimension.spacing.medium",
+    },
+    itemControl: {
+      borderColor: "dic.system.color.component.outline",
+      borderRadius: "dic.system.dimension.radius.full",
+      borderWidth: "dic.system.dimension.border.medium",
+      height: "16px",
+      width: "16px",
+      margin: "4px",
+      _checked: {
+        background: "dic.system.color.impression.primary",
+        outlineColor: "dic.system.color.component.inverseOnSurface",
+        outlineStyle: "solid",
+        outlineWidth: "2px",
+        outlineOffset: "-3px",
+        borderColor: "dic.system.color.impression.primary",
+      },
+      _disabled: {
+        opacity: 0.6,
+      },
+    },
+    itemText: {
+      _disabled: {
+        color: "dic.system.color.interaction.disabledOnSurface",
+      },
+    },
+  },
+  defaultVariants: {
+    variant: "expanded"
+  },
+  variants: {
+    variant: {
+      expanded: {
+        itemText: {
+          fontSize: "dic.reference.typography.scale.expanded.medium"
+        }
+      },
+      compact: {
+        itemText: {
+          fontSize: "dic.reference.typography.scale.expanded.large"
+        }
+      }
     }
   }
-})
+});
 
-const RadioGroupItem = styled(RadioGroup.Item, {
-  base: {
-    alignItems: "center",
-    display: "flex",
-    cursor: "pointer",
-    gap: "dic.system.dimension.spacing.small",
-    paddingY: "dic.system.dimension.spacing.small",
-    paddingX: "dic.system.dimension.spacing.medium"
-  }
-})
+type RadioButtonItemProps = {
+  label: string;
+  description?: string;
+  variant?: "expanded" | "compact";
+};
 
-export const RadioButton = () => {
-  const frameworks = ['React', 'Solid', 'Vue']
+export type RadioButtonProps = RadioGroupItemProps &
+  RadioGroupItemControlProps &
+  RadioGroupItemTextProps &
+  RecipeVariantProps<typeof RadioButtonStyle> &
+  RadioButtonItemProps;
+
+export const RadioButton: React.FC<RadioButtonProps> = ({
+  value,
+  label,
+  description,
+  ...props
+}) => {
+  const styles = RadioButtonStyle(props);
 
   return (
-    <RadioGroup.Root>
-      <RadioGroup.Label>Framework</RadioGroup.Label>
-      <RadioGroup.Indicator />
-      {frameworks.map((framework) => (
-        <RadioGroupItem key={framework} value={framework}>
-          <RadioGroupItemControl />
-          <RadioGroup.ItemText>{framework}</RadioGroup.ItemText>
-        </RadioGroupItem>
-      ))}
-    </RadioGroup.Root>
-  )
-}
+    <RadioGroup.Item
+      key={value}
+      value={value}
+      className={styles.item}
+      {...props}
+    >
+      <RadioGroup.ItemControl className={styles.itemControl} />
+      <RadioGroup.ItemText className={styles.itemText}>
+        {label}
+      </RadioGroup.ItemText>
+      {description && description}
+    </RadioGroup.Item>
+  );
+};
