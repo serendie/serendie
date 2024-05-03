1df46c3b9dda7f97a505e2387c241f0bb9514dff
kenshir0f <kenshir0f.design@gmail.com>
2024-05-03 17:14:31 +0900

    Add helperText props to radio button component

--- ui/src/components/RadioButton.stories.tsx
+++ ui/src/components/RadioButton.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { RadioButton, RadioButtonProps } from "./RadioButton"
+import { RadioButton, RadioButtonProps, RadioButtonStyle } from "./RadioButton";
 import { RadioGroup } from "@ark-ui/react";
 
 const meta: Meta<typeof RadioButton> = {
@@ -10,31 +10,72 @@ const meta: Meta<typeof RadioButton> = {
       <RadioGroup.Root>
         <Story />
       </RadioGroup.Root>
-    )
-  ]
-}
-
+    ),
+  ],
+  argTypes: {
+    variant: {
+      options: RadioButtonStyle.variantMap.variant,
+      control: { type: "radio" },
+      defaultValue: "expanded",
+    },
+    helperText: {
+      control: { type: "text" },
+      defaultValue: "",
+    },
+  },
+};
 
 export default meta;
 type Story = StoryObj<typeof RadioButton>;
 
-const Template= (args: RadioButtonProps) => (
-  <RadioGroup.Root>
-    <RadioButton {...args} label="タイトルタイトル1" value="itemA" />
-    <RadioButton {...args} label="タイトルタイトル2" value="itemB" />
-    <RadioButton {...args} label="タイトルタイトル3" value="itemC" disabled={true} />
-    <RadioButton {...args} label="タイトルタイトル4" value="itemD" />
-  </RadioGroup.Root>
+const Template = (args: RadioButtonProps) => (
+  <>
+    <RadioButton
+      {...args}
+      label="タイトルタイトル1"
+      value="itemA"
+      helperText={args.helperText}
+    />
+    <RadioButton
+      {...args}
+      label="タイトルタイトル2"
+      value="itemB"
+      helperText={args.helperText}
+    />
+    <RadioButton
+      {...args}
+      label="タイトルタイトル3"
+      value="itemC"
+      disabled={true}
+    />
+    <RadioButton
+      {...args}
+      label="タイトルタイトル4"
+      value="itemD"
+      helperText={args.helperText}
+    />
+  </>
 );
-
 
 export const Expanded: Story = {
   render: Template,
+  args: {
+    variant: "expanded",
+  },
 };
 
 export const Compact: Story = {
   render: Template,
   args: {
     variant: "compact",
+  },
+};
+
+export const WithHelperText: Story = {
+  render: Template,
+  args: {
+    variant: "expanded",
+    helperText:
+      "補足テキスト補足テキスト補足テキスト補足テキスト補足テキスト補足テキスト",
   },
 };
--- ui/src/components/RadioButton.tsx
+++ ui/src/components/RadioButton.tsx
@@ -6,65 +6,90 @@ import {
 } from "@ark-ui/react";
 
 import { RecipeVariantProps, sva } from "../../styled-system/css";
+import { CSSProperties } from "react";
 
 export const RadioButtonStyle = sva({
-  slots: ["item", "itemControl", "itemText"],
+  slots: ["item", "itemControl", "itemTextGroup", "itemText", "helperText"],
   base: {
     item: {
       alignItems: "center",
       display: "flex",
       cursor: "pointer",
-      gap: "dic.system.dimension.spacing.small",
+      gap: "dic.system.dimension.spacing.medium",
       paddingY: "dic.system.dimension.spacing.small",
       paddingX: "dic.system.dimension.spacing.medium",
     },
     itemControl: {
       borderColor: "dic.system.color.component.outline",
       borderRadius: "dic.system.dimension.radius.full",
-      borderWidth: "dic.system.dimension.border.medium",
-      height: "16px",
-      width: "16px",
-      margin: "4px",
+      borderWidth: "1.5px",
+      height: "17.5px",
+      width: "17.5px",
+      margin: "3.25px",
+      flexShrink: 0,
       _checked: {
         background: "dic.system.color.impression.primary",
         outlineColor: "dic.system.color.component.inverseOnSurface",
         outlineStyle: "solid",
-        outlineWidth: "2px",
-        outlineOffset: "-3px",
+        outlineWidth: "1.25px",
+        outlineOffset: "-2.5px",
         borderColor: "dic.system.color.impression.primary",
       },
       _disabled: {
         opacity: 0.6,
       },
     },
+    itemTextGroup: {
+      display: "flex",
+      flexFlow: "column",
+    },
     itemText: {
+      fontFamily: "dic.reference.typography.fontFamily.primary",
+      color: "dic.system.color.component.onSurface",
+      lineHeight: 1,
       _disabled: {
         color: "dic.system.color.interaction.disabledOnSurface",
       },
     },
+    helperText: {
+      fontFamily: "dic.reference.typography.fontFamily.primary",
+      color: "dic.system.color.component.onSurfaceVariant",
+      marginTop: "dic.system.dimension.spacing.twoExtraSmall",
+      lineHeight: 1.4,
+      _disabled: {
+        color: "dic.system.color.interaction.disabledOnSurface",
+      }
+    },
   },
   defaultVariants: {
-    variant: "expanded"
+    variant: "expanded",
   },
+  // TODO: system.typography.body を参照できないため後で差し替える
   variants: {
     variant: {
       expanded: {
         itemText: {
-          fontSize: "dic.reference.typography.scale.expanded.medium"
-        }
+          fontSize: "dic.reference.typography.scale.expanded.small",
+        },
+        helperText: {
+          fontSize: "dic.reference.typography.scale.expanded.twoExtraSmall",
+        },
       },
       compact: {
         itemText: {
-          fontSize: "dic.reference.typography.scale.expanded.large"
-        }
-      }
-    }
-  }
+          fontSize: "dic.reference.typography.scale.compact.large",
+        },
+        helperText: {
+          fontSize: "dic.reference.typography.scale.compact.small",
+        },
+      },
+    },
+  },
 });
 
 type RadioButtonItemProps = {
   label: string;
-  description?: string;
+  helperText?: string;
   variant?: "expanded" | "compact";
 };
 
@@ -77,23 +102,33 @@ export type RadioButtonProps = RadioGroupItemProps &
 export const RadioButton: React.FC<RadioButtonProps> = ({
   value,
   label,
-  description,
+  helperText,
   ...props
 }) => {
   const styles = RadioButtonStyle(props);
+  const itemStyle: CSSProperties = helperText
+    ? { alignItems: "flex-start" }
+    : {};
 
   return (
     <RadioGroup.Item
       key={value}
       value={value}
       className={styles.item}
+      style={itemStyle}
       {...props}
     >
       <RadioGroup.ItemControl className={styles.itemControl} />
-      <RadioGroup.ItemText className={styles.itemText}>
-        {label}
-      </RadioGroup.ItemText>
-      {description && description}
+      <div className={styles.itemTextGroup}>
+        <RadioGroup.ItemText className={styles.itemText}>
+          {label}
+        </RadioGroup.ItemText>
+        {helperText && (
+          <RadioGroup.ItemText className={styles.helperText}>
+            {helperText}
+          </RadioGroup.ItemText>
+        )}
+      </div>
     </RadioGroup.Item>
   );
 };
