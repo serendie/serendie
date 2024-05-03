8e82f19f3161b40e90385f7d2fcbfdc855a6d091
kenshir0f <kenshir0f.design@gmail.com>
2024-05-03 18:22:41 +0900

    Replace item control icon from CSS to svg

--- ui/src/components/RadioButton.tsx
+++ ui/src/components/RadioButton.tsx
@@ -4,39 +4,46 @@ import {
   RadioGroupItemProps,
   RadioGroupItemTextProps,
 } from "@ark-ui/react";
-
 import { RecipeVariantProps, sva } from "../../styled-system/css";
 import { CSSProperties } from "react";
 
 export const RadioButtonStyle = sva({
-  slots: ["item", "itemControl", "itemTextGroup", "itemText", "helperText"],
+  slots: [
+    "item",
+    "itemControl",
+    "checkedIcon",
+    "itemTextGroup",
+    "itemText",
+    "helperText",
+  ],
   base: {
     item: {
-      alignItems: "center",
       display: "flex",
-      cursor: "pointer",
+      alignItems: "center",
       gap: "dic.system.dimension.spacing.medium",
       paddingY: "dic.system.dimension.spacing.small",
       paddingX: "dic.system.dimension.spacing.medium",
+      cursor: "pointer",
     },
     itemControl: {
-      borderColor: "dic.system.color.component.outline",
-      borderRadius: "dic.system.dimension.radius.full",
-      borderWidth: "1.5px",
-      height: "17.5px",
-      width: "17.5px",
-      margin: "3.25px",
       flexShrink: 0,
-      _checked: {
-        background: "dic.system.color.impression.primary",
-        outlineColor: "dic.system.color.component.inverseOnSurface",
-        outlineStyle: "solid",
-        outlineWidth: "1.25px",
-        outlineOffset: "-2.5px",
-        borderColor: "dic.system.color.impression.primary",
-      },
       _disabled: {
         opacity: 0.6,
+      },
+    },
+    checkedIcon: {
+      width: "dic.reference.dimension.scale.8",
+      height: "dic.reference.dimension.scale.8",
+      color: "dic.system.color.component.outline",
+      // ark-ui の checked のプロパティの取得方法が不明なため、暫定対応でスタイルで調整
+      "& .checkedCircle": {
+        opacity: 0,
+      },
+      _checked: {
+        color: "dic.system.color.impression.primary",
+        "& .checkedCircle": {
+          opacity: 1,
+        },
       },
     },
     itemTextGroup: {
@@ -46,7 +53,7 @@ export const RadioButtonStyle = sva({
     itemText: {
       fontFamily: "dic.reference.typography.fontFamily.primary",
       color: "dic.system.color.component.onSurface",
-      lineHeight: 1,
+      lineHeight: "dic.reference.typography.lineHeight.tight",
       _disabled: {
         color: "dic.system.color.interaction.disabledOnSurface",
       },
@@ -55,16 +62,16 @@ export const RadioButtonStyle = sva({
       fontFamily: "dic.reference.typography.fontFamily.primary",
       color: "dic.system.color.component.onSurfaceVariant",
       marginTop: "dic.system.dimension.spacing.twoExtraSmall",
-      lineHeight: 1.4,
+      lineHeight: "dic.reference.typography.lineHeight.tight",
       _disabled: {
         color: "dic.system.color.interaction.disabledOnSurface",
-      }
+      },
     },
   },
   defaultVariants: {
     variant: "expanded",
   },
-  // TODO: system.typography.body を参照できないため後で差し替える
+  // TODO: fontSize で system.typography.body を参照できないため後で差し替える
   variants: {
     variant: {
       expanded: {
@@ -118,7 +125,9 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
       style={itemStyle}
       {...props}
     >
-      <RadioGroup.ItemControl className={styles.itemControl} />
+      <RadioGroup.ItemControl className={styles.itemControl} asChild>
+        <CheckedIcon className={styles.checkedIcon} />
+      </RadioGroup.ItemControl>
       <div className={styles.itemTextGroup}>
         <RadioGroup.ItemText className={styles.itemText}>
           {label}
@@ -132,3 +141,26 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
     </RadioGroup.Item>
   );
 };
+
+// TODO: アイコンコンポーネントは後で別ファイルに切り出す
+interface IconProps extends React.SVGProps<SVGSVGElement> {}
+
+const CheckedIcon: React.FC<IconProps> = (props) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M12 4.75C7.99594 4.75 4.75 7.99594 4.75 12C4.75 16.0041 7.99594 19.25 12 19.25C16.0041 19.25 19.25 16.0041 19.25 12C19.25 7.99594 16.0041 4.75 12 4.75ZM3.25 12C3.25 7.16751 7.16751 3.25 12 3.25C16.8325 3.25 20.75 7.16751 20.75 12C20.75 16.8325 16.8325 20.75 12 20.75C7.16751 20.75 3.25 16.8325 3.25 12Z"
+      fill="currentColor"
+    />
+    <path
+      d="M18 12C18 15.3137 15.3137 18 12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12Z"
+      fill="currentColor"
+      className="checkedCircle"
+    />
+  </svg>
+);
