ec16df55095c9ddc14ce3474909f6c0b338d62bf
kenshir0f <kenshir0f.design@gmail.com>
2024-05-19 21:51:19 +0900

    Add success type to toast

--- ui/src/components/Toast.stories.tsx
+++ ui/src/components/Toast.stories.tsx
@@ -1,31 +1,51 @@
-import type { Meta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import { Toast, toaster } from "./Toast";
 import { Button } from "./Button";
-
-const Template = () => (
-  <div>
-    <Button
-      size="medium"
-      onClick={() =>
-        toaster.create({
-          title: "トーストトーストトースト",
-          duration: 1000
-        })
-      }
-    >
-      Show Toast
-    </Button>
-    <Toast />
-  </div>
-);
-
-export const Default = Template;
 
 const meta: Meta<typeof Toast> = {
   component: Toast,
   decorators: [(Story) => <Story />],
 };
+type Story = StoryObj<typeof Toast>;
 
 export default meta;
 
+export const Default: Story = {
+  render: () => (
+    <div>
+      <Button
+        size="medium"
+        onClick={() =>
+          toaster.create({
+            title: "トーストトーストトースト",
+            duration: 3000,
+          })
+        }
+      >
+        Show Toast
+      </Button>
+      <Toast />
+    </div>
+  ),
+};
+
+export const WithIcon: Story = {
+  render: () => (
+    <div>
+      <Button
+        size="medium"
+        onClick={() =>
+          toaster.create({
+            title: "トーストトーストトースト",
+            duration: 3000,
+            type: "success",
+          })
+        }
+      >
+        Show Toast
+      </Button>
+      <Toast />
+    </div>
+  ),
+};
--- ui/src/components/Toast.tsx
+++ ui/src/components/Toast.tsx
@@ -1,21 +1,35 @@
 import { sva } from "../../styled-system/css";
 import { Toast as ArkToast, createToaster } from "@ark-ui/react";
+import CheckIcon from "../assets/check.svg?react";
 
 export const ToastStyle = sva({
-  slots: ["root", "text"],
+  slots: ["root", "textGroup", "icon", "text"],
   base: {
     root: {
+      display: "flex",
+      alignItems: "center",
       background: "dic.system.color.component.inverseSurface",
       paddingX: "dic.system.dimension.spacing.extraLarge",
-      paddingY: "dic.system.dimension.spacing.small",
       borderRadius: "dic.system.dimension.radius.medium",
+      boxShadow: "dic.system.elevation.shadow.level3",
+      height: 40,
+    },
+    textGroup: {
+      display: "flex",
+      alignItems: "center",
+      gap: "dic.system.dimension.spacing.twoExtraSmall",
+    },
+    icon: {
+      width: 24,
+      height: 24,
+      color: "dic.system.color.impression.positive",
     },
     text: {
       color: "dic.system.color.component.inverseOnSurface",
       textStyle: "dic.system.typography.body.small_compact",
       _expanded: {
         textStyle: "dic.system.typography.body.small_expanded",
-      }
+      },
     },
   },
 });
@@ -27,7 +41,10 @@ const [Toast, toaster] = createToaster({
 
     return (
       <ArkToast.Root key={toast.rootProps.id} className={styles.root}>
-        <ArkToast.Title className={styles.text}>{toast.title}</ArkToast.Title>
+        <div className={styles.textGroup}>
+          {toast.type === "success" && <CheckIcon className={styles.icon} />}
+          <ArkToast.Title className={styles.text}>{toast.title}</ArkToast.Title>
+        </div>
       </ArkToast.Root>
     );
   },
