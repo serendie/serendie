219ca7ea8a3330a9296460b2a5594c90151bdd52
kenshir0f <kenshir0f.design@gmail.com>
2024-05-19 17:17:13 +0900

    Refactor badge style

--- ui/src/components/NotificationBadge.tsx
+++ ui/src/components/NotificationBadge.tsx
@@ -1,35 +1,47 @@
-import { cva } from "../../styled-system/css";
+import { RecipeVariantProps, sva } from "../../styled-system/css";
 
-const NotificationBadgeStyle = cva({
+const NotificationBadgeStyle = sva({
+  slots: ["root", "text"],
   base: {
-    position: "absolute",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    height: 16,
-    minWidth: 16,
-    color: "dic.system.color.impression.onNegative",
-    borderRadius: "dic.system.dimension.radius.full",
-    paddingX: "dic.system.dimension.spacing.twoExtraSmall",
-    paddingBottom: 2,
-    textStyle: "dic.system.typography.label.small_compact",
-    _expanded: {
-      textStyle: "dic.system.typography.label.small_expanded",
+    root: {
+      position: "absolute",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: 16,
+      minWidth: 16,
+      color: "dic.system.color.impression.onNegative",
+      borderRadius: "dic.system.dimension.radius.full",
+      paddingX: "dic.system.dimension.spacing.twoExtraSmall",
+      paddingBottom: 2,
+      textStyle: "dic.system.typography.label.small_compact",
+      _expanded: {
+        textStyle: "dic.system.typography.label.small_expanded",
+      },
+    },
+    text: {
+      height: 11,
     },
   },
   variants: {
     variant: {
       primary: {
-        backgroundColor: "dic.system.color.impression.negative",
+        root: {
+          backgroundColor: "dic.system.color.impression.negative",
+        },
       },
       secondary: {
-        backgroundColor: "dic.system.color.impression.primary",
+        root: {
+          backgroundColor: "dic.system.color.impression.primary",
+        },
       },
     },
     noNumber: {
       true: {
-        minW: 8,
-        height: 8,
+        root: {
+          height: 8,
+          minWidth: 8,
+        },
       },
     },
   },
@@ -38,33 +50,33 @@ const NotificationBadgeStyle = cva({
   },
 });
 
-export type NotificationBadgeProps = {
+type BadgeProps = {
   count: number;
-  noNumber?: boolean;
-  variant?: "primary" | "secondary";
 };
+
+export type NotificationBadgeProps = BadgeProps &
+  RecipeVariantProps<typeof NotificationBadgeStyle>;
 
 export const NotificationBadge: React.FC<NotificationBadgeProps> = ({
   count,
+  noNumber,
   ...props
 }) => {
   const [cssProps, componentProps] =
     NotificationBadgeStyle.splitVariantProps(props);
-  const styles = NotificationBadgeStyle(cssProps);
+  const styles = NotificationBadgeStyle({ noNumber, ...cssProps });
 
-  if (props.noNumber) {
-    return <div className={styles} {...componentProps}></div>;
+  if (noNumber) {
+    return <div className={styles.root} {...componentProps}></div>;
   }
 
-  if (count === 0) {
+  if (count < 1) {
     return null;
   }
 
   return (
-    <div className={styles} {...componentProps}>
-      <span style={{ height: 11 }}>
-        {count > 99 ? "99+" : count > 0 ? count : null}
-      </span>
+    <div className={styles.root} {...componentProps}>
+      <span className={styles.text}>{count > 99 ? "99+" : count}</span>
     </div>
   );
 };
