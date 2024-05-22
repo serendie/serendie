2d667d725f6ae42d65a70125b649d10e457c7170
nagayama <nagayama@iroirodesign.com>
2024-05-22 18:26:45 +0900

    apply styles

--- ui/src/components/ModalDialog.tsx
+++ ui/src/components/ModalDialog.tsx
@@ -6,13 +6,69 @@ import { Button } from "./Button";
 
 const ModalDialogStyle = sva({
   slots: [
-    "root",
+    "backdrop",
     "content",
+    "contentInner",
     "title",
     "description",
     "buttonWrapper",
     "closeTrigger",
   ],
+  base: {
+    backdrop: {
+      background: "dic.system.color.component.scrim",
+      position: "fixed",
+      inset: 0,
+      zIndex: "dic.system.elevation.zIndex.modal",
+    },
+    content: {
+      position: "fixed",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: "calc(100% - {spacing.dic.system.dimension.spacing.large} * 2)",
+      maxWidth: "408px",
+      display: "grid",
+      gap: "dic.system.dimension.spacing.twoExtraLarge",
+      paddingTop: {
+        base: "dic.system.dimension.spacing.extraLarge",
+        expanded: "dic.system.dimension.spacing.large",
+      },
+      paddingRight: "dic.system.dimension.spacing.extraLarge",
+      paddingBottom: "dic.system.dimension.spacing.large",
+      paddingLeft: "dic.system.dimension.spacing.extraLarge",
+      backgroundColor: "dic.system.color.component.surface",
+      boxShadow: "dic.system.elevation.shadow.level5",
+      borderRadius: "dic.system.dimension.radius.medium",
+      zIndex: "dic.system.elevation.zIndex.modal",
+    },
+    contentInner: {
+      display: "grid",
+      gap: "dic.system.dimension.spacing.medium",
+    },
+    title: {
+      textStyle: {
+        base: "dic.system.typography.title.small_compact",
+        expanded: "dic.system.typography.title.small_expanded",
+      },
+    },
+    description: {
+      textStyle: {
+        base: "dic.system.typography.body.medium_compact",
+        expanded: "dic.system.typography.body.medium_expanded",
+      },
+    },
+    buttonWrapper: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      gap: "dic.system.dimension.spacing.medium",
+      expanded: {
+        flexDirection: "row-reverse",
+        justifyContent: "end",
+      },
+    },
+  },
 });
 
 type Props = {
@@ -37,19 +93,26 @@ export const ModalDialog: React.FC<ModalDialogProps> = ({
   children,
   ...rest
 }) => {
+  const styles = ModalDialogStyle(rest);
   return (
     <Dialog.Root open={isOpen} {...rest}>
       <Portal>
-        <Dialog.Backdrop />
+        <Dialog.Backdrop className={styles.backdrop} />
         <Dialog.Positioner>
-          <Dialog.Content>
-            <Dialog.Title>{title}</Dialog.Title>
-            <Dialog.Description>{children}</Dialog.Description>
-            <div>
-              <Dialog.CloseTrigger asChild>
-                <Button>{closeButtonLabel || "閉じる"}</Button>
-              </Dialog.CloseTrigger>
+          <Dialog.Content className={styles.content}>
+            <div className={styles.contentInner}>
+              <Dialog.Title className={styles.title}>{title}</Dialog.Title>
+              <Dialog.Description className={styles.description}>
+                {children}
+              </Dialog.Description>
+            </div>
+            <div className={styles.buttonWrapper}>
               <Button onClick={onButtonClick}>{buttonLabel}</Button>
+              <Dialog.CloseTrigger asChild>
+                <Button styleType="ghost">
+                  {closeButtonLabel || "閉じる"}
+                </Button>
+              </Dialog.CloseTrigger>
             </div>
           </Dialog.Content>
         </Dialog.Positioner>
