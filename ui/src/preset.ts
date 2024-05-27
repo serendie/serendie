0db99e74d446bb98d096e1f489b93f270115076f
nagayama <nagayama@iroirodesign.com>
2024-05-27 23:32:22 +0900

    tidy up

--- ui/src/preset.ts
+++ ui/src/preset.ts
@@ -1,17 +1,9 @@
-import { Preset, TextStyles } from "@pandacss/dev";
+import { Preset } from "@pandacss/dev";
 import { SpreadRecipes } from "./recipes";
 import { SpreadTokens, SpreadTypography } from "./tokens";
 import { getToken } from "./tokens/getToken";
 
 const { dic } = getToken();
-
-const bodyTextStyle: TextStyles = {
-  body: {
-    value: {
-      fontFamily: `'Roboto Flex Variable','Noto Sans JP Variable', sans-serif`,
-    },
-  },
-};
 
 export const SpreadPreset: Preset = {
   theme: {
@@ -27,7 +19,6 @@ export const SpreadPreset: Preset = {
         },
       },
       textStyles: {
-        bodyTextStyle,
         ...SpreadTypography,
       },
     },
