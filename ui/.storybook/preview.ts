363ea8578d4c6df877d8b63df74ad2a40faf8d38
nagayama <nagayama@iroirodesign.com>
2024-05-29 13:01:43 +0900

    update storybook

--- ui/.storybook/preview.ts
+++ ui/.storybook/preview.ts
@@ -4,7 +4,6 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -12,6 +11,8 @@ const preview: Preview = {
       },
     },
   },
+
+  tags: ["autodocs"]
 };
 
 export default preview;
