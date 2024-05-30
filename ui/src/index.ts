44c49eb5012e78f8a33f85e367a3c5469dff42d0
nagayama <nagayama@iroirodesign.com>
2024-05-30 15:33:06 +0900

    layout basic

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,5 +1,17 @@
-export { SvgIcon } from "./components/SvgIcon";
-export { Switch } from "./components/Switch";
-export { Button } from "./components/Button";
-export { Badge } from "./components/Badge";
 export { SpreadPreset } from "./preset";
+
+export * from "./components/Badge.tsx";
+export * from "./components/Button.tsx";
+export * from "./components/Checkbox.tsx";
+export * from "./components/Divider.tsx";
+export * from "./components/IconButton.tsx";
+export * from "./components/ModalDialog.tsx";
+export * from "./components/NotificationBadge.tsx";
+export * from "./components/RadioButton.tsx";
+export * from "./components/RadioGroup.tsx";
+export * from "./components/Search.tsx";
+export * from "./components/Select.tsx";
+export * from "./components/SvgIcon.tsx";
+export * from "./components/Switch.tsx";
+export * from "./components/TextArea.tsx";
+export * from "./components/TextField.tsx";
