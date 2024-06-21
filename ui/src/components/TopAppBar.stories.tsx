daa1633f4b8c7219f6291b50ebdd1a1bbd07f0b9
yahiro <133099806+centeringbook@users.noreply.github.com>
2024-06-21 11:14:50 +0900

    Fix Storybook

--- ui/src/components/TopAppBar.stories.tsx
+++ ui/src/components/TopAppBar.stories.tsx
@@ -9,100 +9,101 @@ const meta: Meta<typeof TopAppBar> = {
       expanded: true,
     },
   },
-  args: {},
 };
 type Story = StoryObj<typeof TopAppBar>;
 
-export const All: Story = {
+export const All: StoryObj<typeof AllTemplate> = {
+  args: {
+    arg1: {
+      type: "navbar",
+      headingIconButton: (
+        <>
+          <IconButton shape="rectangle" styleType="ghost" icon="menu" />
+        </>
+      ),
+      trailingIconButtons: (
+        <>
+          <IconButton shape="rectangle" styleType="ghost" icon="search" />
+          <IconButton shape="rectangle" styleType="ghost" icon="add" />
+          <IconButton shape="rectangle" styleType="ghost" icon="info" />
+        </>
+      ),
+    },
+    arg2: {
+      type: "titleBar",
+      title: "Title Bar",
+      headingIconButton: (
+        <>
+          <IconButton shape="rectangle" styleType="ghost" icon="chevron_left" />
+        </>
+      ),
+      trailingIconButtons: (
+        <>
+          <IconButton shape="rectangle" styleType="ghost" icon="add" />
+        </>
+      ),
+    },
+  },
   render: (args) => {
-    return (
-      <>
-        <TopAppBar
-          type={"navbar"}
-          headingIconButton={
-            <>
-              <IconButton shape="rectangle" styleType="ghost" icon="menu" />
-            </>
-          }
-          trailingIconButtons={
-            <>
-              <IconButton shape="rectangle" styleType="ghost" icon="search" />
-              <IconButton shape="rectangle" styleType="ghost" icon="add" />
-              <IconButton shape="rectangle" styleType="ghost" icon="info" />
-            </>
-          }
-        />
-        <TopAppBar
-          type={"titleBar"}
-          title="Title Bar"
-          headingIconButton={
-            <>
-              <IconButton
-                shape="rectangle"
-                styleType="ghost"
-                icon="chevron_left"
-              />
-            </>
-          }
-          trailingIconButtons={
-            <>
-              <IconButton shape="rectangle" styleType="ghost" icon="add" />
-            </>
-          }
-        />
-      </>
-    );
+    return <AllTemplate {...args} />;
   },
 };
 
+type AllTemplateProps = {
+  arg1: {
+    type: "navbar";
+    headingIconButton: React.ReactNode;
+    trailingIconButtons: React.ReactNode;
+  };
+  arg2: {
+    type: "titleBar";
+    title: string;
+    headingIconButton: React.ReactNode;
+    trailingIconButtons: React.ReactNode;
+  };
+};
+
+const AllTemplate: React.FC<AllTemplateProps> = ({ arg1, arg2 }) => {
+  return (
+    <>
+      <TopAppBar {...arg1} />
+      <TopAppBar {...arg2} />
+    </>
+  );
+};
+
 export const Navbar: Story = {
-  render: (args) => {
-    return (
+  args: {
+    type: "navbar",
+    headingIconButton: (
+      <IconButton shape="rectangle" styleType="ghost" icon="menu" />
+    ),
+    trailingIconButtons: (
       <>
-        <TopAppBar
-          type={"navbar"}
-          headingIconButton={
-            <>
-              <IconButton shape="rectangle" styleType="ghost" icon="menu" />
-            </>
-          }
-          trailingIconButtons={
-            <>
-              <IconButton shape="rectangle" styleType="ghost" icon="search" />
-              <IconButton shape="rectangle" styleType="ghost" icon="add" />
-              <IconButton shape="rectangle" styleType="ghost" icon="info" />
-            </>
-          }
-        />
+        <IconButton shape="rectangle" styleType="ghost" icon="search" />
+        <IconButton shape="rectangle" styleType="ghost" icon="add" />
+        <IconButton shape="rectangle" styleType="ghost" icon="info" />
       </>
-    );
+    ),
+  },
+  render: (args) => {
+    return <TopAppBar {...args} />;
   },
 };
 
 export const Title: Story = {
+  args: {
+    type: "titleBar",
+    title: "Title Bar",
+    headingIconButton: (
+      <IconButton shape="rectangle" styleType="ghost" icon="chevron_left" />
+    ),
+    trailingIconButtons: (
+      <IconButton shape="rectangle" styleType="ghost" icon="add" />
+    ),
+  },
   render: (args) => {
-    return (
-      <>
-        <TopAppBar
-          type={"titleBar"}
-          title="Title Bar"
-          headingIconButton={
-            <>
-              <IconButton
-                shape="rectangle"
-                styleType="ghost"
-                icon="chevron_left"
-              />
-            </>
-          }
-          trailingIconButtons={
-            <>
-              <IconButton shape="rectangle" styleType="ghost" icon="add" />
-            </>
-          }
-        />
-      </>
-    );
+    return <TopAppBar {...args} />;
   },
 };
 
