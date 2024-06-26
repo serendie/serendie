f939b8c8ef8c6ac74638c3d534f18887f994628e
degoo <degoo@takram.com>
2024-06-26 21:43:11 +0900

    Export ui

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -2,6 +2,7 @@ export { SerendiePreset } from "./preset";
 
 export * from "./components/Accordion.tsx";
 export * from "./components/AccordionGroup.tsx";
+export * from "./components/Avatar.tsx";
 export * from "./components/Badge.tsx";
 export * from "./components/Banner.tsx";
 export * from "./components/BottomNavigation.tsx";
@@ -11,6 +12,7 @@ export * from "./components/Checkbox.tsx";
 export * from "./components/DashboardWidget.tsx";
 export * from "./components/Divider.tsx";
 export * from "./components/Drawer.tsx";
+export * from "./components/DropdownMenu.tsx";
 export * from "./components/IconButton.tsx";
 export * from "./components/List.tsx";
 export * from "./components/ListItem.tsx";
