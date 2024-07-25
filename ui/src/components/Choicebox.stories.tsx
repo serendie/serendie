0f9de43df37ec2afbdc9d98743c211f58a2d160a
degoo <degoo@takram.com>
2024-07-25 22:09:37 +0900

    Apply to Choicebox

--- ui/src/components/Choicebox.stories.tsx
+++ ui/src/components/Choicebox.stories.tsx
@@ -1,12 +1,38 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
 import { Choicebox } from "./Choicebox";
 import { RadioGroup } from "..";
+import figma from "@figma/code-connect";
 
 const meta: Meta<typeof Choicebox> = {
   component: Choicebox,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/8oZpZ2xolRhCUPDGSlWXr0/Serendie-Design-System?node-id=6816-45671",
+      props: {
+        type: figma.enum("Type", {
+          Radio: "radio",
+          Checkbox: "checkbox",
+        }),
+        checked: figma.enum("State", { Selected: true }),
+        disabled: figma.enum("State", {
+          "Disabled-Enabled": true,
+          "Disabled-Selected": true,
+        }),
+      },
+      examples: [FigmaExample],
+    },
+  },
   decorators: [(Story) => <Story />],
 };
+
+function FigmaExample(props: React.ComponentProps<typeof Choicebox>) {
+  return (
+    <RadioGroup>
+      <Choicebox {...props} value="itemA" />
+    </RadioGroup>
+  );
+}
 
 export default meta;
 type Story = StoryObj<typeof Choicebox>;
