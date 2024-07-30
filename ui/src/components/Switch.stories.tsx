09f156af1fdc3bdde0bd3efe56848f4f5a3056d2
degoo <degoo@takram.com>
2024-07-30 19:54:35 +0900

    Apply to Switch

--- ui/src/components/Switch.stories.tsx
+++ ui/src/components/Switch.stories.tsx
@@ -1,9 +1,34 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
 import { Switch, SwitchProps } from "./Switch";
+import figma from "@figma/code-connect";
+
+const Template = (args: SwitchProps) => (
+  <Switch {...args} label="タイトルタイトル1" helperText={args.helperText} />
+);
 
 const meta: Meta<typeof Switch> = {
   component: Switch,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/8oZpZ2xolRhCUPDGSlWXr0/Serendie-Design-System?node-id=3311-28493",
+      props: {
+        label: figma.string("Label"),
+        helperText: figma.enum("Line", {
+          Multiple: figma.string("HelperText"),
+        }),
+        disabled: figma.enum("State", {
+          Disabled: true,
+          "Disabled-Selected": true,
+        }),
+        checked: figma.enum("State", {
+          Selected: true,
+          "Disabled-Selected": true,
+        }),
+      },
+      examples: [FigmaExample],
+    },
+  },
   decorators: [(Story) => <Story />],
   argTypes: {
     helperText: {
@@ -13,16 +38,12 @@ const meta: Meta<typeof Switch> = {
   },
 };
 
+function FigmaExample(props: React.ComponentProps<typeof Switch>) {
+  return <Switch {...props} />;
+}
+
 export default meta;
 type Story = StoryObj<typeof Switch>;
-
-const Template = (args: SwitchProps) => (
-  <Switch
-    {...args}
-    label="タイトルタイトル1"
-    helperText={args.helperText}
-  />
-);
 
 export const Default: Story = {
   render: Template,
