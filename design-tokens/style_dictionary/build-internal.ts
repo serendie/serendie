f2527f7bcc8ba0715da41bb86c1c1e4772fcc290
nagayama <nagayama@iroirodesign.com>
2024-10-01 10:10:36 +0900

    とりあえずファイルを出し分けてinternalとそうでないものを出力

---
+++ design-tokens/style_dictionary/build-internal.ts
@@ -0,0 +1,66 @@
+import StyleDictionary from "style-dictionary-utils";
+import { SerendieParser } from "./parser";
+import "./formatter";
+import "./transformer/cssShadow";
+import "./transformer/cssTypography";
+import { customFileHeader } from "./customFileHeader";
+
+StyleDictionary.registerParser(SerendieParser);
+
+StyleDictionary.registerFilter({
+  name: "onlyInternal",
+  matcher: (token) => token.filePath.includes("internal"),
+});
+
+const internalDictionary = StyleDictionary.extend({
+  source: ["tokens/**/*.json"],
+  platforms: {
+    css: {
+      buildPath: "dist/internal/",
+      options: {
+        fileHeader: customFileHeader,
+      },
+      transforms: [
+        "attribute/cti",
+        "name/cti/kebab",
+        "color/css",
+        "cssShadow",
+        "cssTypography",
+      ],
+      files: [
+        {
+          destination: "tokens.css",
+          format: "css/variables",
+          filter: "onlyInternal",
+        },
+      ],
+    },
+    js: {
+      buildPath: "dist/internal/",
+      options: {
+        fileHeader: customFileHeader,
+      },
+      transforms: [
+        "attribute/cti",
+        "name/cti/camel",
+        "time/seconds",
+        "content/icon",
+        "color/css",
+      ],
+      files: [
+        {
+          destination: "panda-tokens.js",
+          format: "panda-css-module",
+          filter: "onlyInternal",
+        },
+        {
+          destination: "panda-tokens.d.ts",
+          format: "panda-css-module-declarations",
+          filter: "onlyInternal",
+        },
+      ],
+    },
+  },
+});
+
+internalDictionary.buildAllPlatforms();
