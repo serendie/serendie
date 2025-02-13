0559d94bad42caf7561002ab2de75e4a73f2eba7
nagayama <nagayama@iroirodesign.com>
2025-02-13 17:53:32 +0900

    tokens.sizesとtokens.spacingをdeep mergeする

--- ui/src/tokens/index.ts
+++ ui/src/tokens/index.ts
@@ -1,9 +1,16 @@
 import serendieTokens from "@serendie/design-token/panda";
+import merge from "deepmerge";
 
 // PandaCSSのtokensとtextStylesが混在しているので分離する
 const { textStyles, ...tokens } = serendieTokens;
 
 // tokensの中からspacingを取り出してsizesとして定義する
-export const SerendieTokens = { ...tokens, sizes: tokens.spacing };
+
+const sizes = merge(tokens.sizes, tokens.spacing);
+
+export const SerendieTokens = {
+  ...tokens,
+  sizes,
+};
 
 export const SerendieTypography = textStyles;
