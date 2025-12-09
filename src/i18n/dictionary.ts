1882731ef75ef40fc23469f1d8a188d06469445f
nagayama <8455+nagayama@users.noreply.github.com>
2025-12-09 03:27:49 +0000

    chore: Figma ロケールの同期

--- src/i18n/dictionary.ts
+++ src/i18n/dictionary.ts
@@ -1,10 +1,12 @@
 export const dictionary = {
   ja: {
     "common.required": "必須",
+    "common.test": "テスト",
     "pagination.page": "{{page}}ページ目",
   },
   en: {
     "common.required": "Required",
+    "common.test": "test",
     "pagination.page": "Page {{page}}",
   },
 } as const;
